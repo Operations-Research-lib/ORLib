4f347898c18c32eb17f70da93673614eb6c7dd7a
JirlOP <jorge.diazsagot@ucr.ac.cr>
2023-12-17 18:33:30 -0600

    Add unit test for mm1_model_compute_rho function in queueing_theory module

---
+++ src/test_queueing_theory.py
@@ -0,0 +1,27 @@
+"""Module providing unit test for queueing_theory"""
+import math as ma
+
+import queueing_theory as qt
+
+
+class TestMm1:
+    """Class to test the M/M/1 queueing model"""
+
+    def test_mm1_model_compute_rho(self):
+        """
+        Test case for mm1_model_compute_rho function.
+
+        This function tests the calculation of rho (traffic intensity) in the M/M/1
+        queueing model.
+        It verifies that the calculated value of rho is not equal to 0 and is
+        approximately equal to 0.8.
+
+        """
+        lam_input = 8
+        miu_input = 10
+        expected_output = 0.8
+        rho_calc = qt.mm1_model_compute_rho(lam = lam_input, miu = miu_input)
+
+        assert rho_calc != 0, "rho is equal to 0"
+        assert ma.isclose(rho_calc, expected_output,
+                          rel_tol=1e-9), "rho is not approximately equal to 0.8"
