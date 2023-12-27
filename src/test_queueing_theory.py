24991e5d3779cb34ba5c7149661f6b571148add7
jop <jorge.diazsagot@ucr.ac.cr>
2023-12-27 15:20:47 -0600

    Add unit tests for mm1_model_compute_lq

--- src/test_queueing_theory.py
+++ src/test_queueing_theory.py
@@ -47,6 +47,60 @@ class TestMm1:
             assert str(exception_info.value) == qt.NEGATIVE_INPUT_ERROR
 
 
+
+    def test_mm1_model_compute_lq_expected_valid_output(self):
+        """
+        Test case for mm1_model_compute_lq function.
+
+        This function tests the calculation of Lq in the M/M/1 queueing model.
+        """
+        lam_input = 20
+        miu_input = 30
+        expected_output = 4/3
+
+        lq_calc = qt.mm1_model_compute_lq(lam = lam_input, miu = miu_input)
+
+        assert lq_calc != 0, "Lq is equal to 0"
+        assert ma.isclose(lq_calc, expected_output,
+                          rel_tol=1e-9), "Lq is not approximately equal to 1,333..."
+
+
+    def test_mm1_model_compute_lq_unstable_system_input_raises_exception(self):
+        """
+        Test case for mm1_model_compute_lq function.
+
+        This function test if the input raise an exception when the system is
+        unstable.
+        """
+        # arrange
+        input_cases = [(10,10), (10,5)]
+
+        for lam_input, miu_input in input_cases:
+            with raises(Exception) as exception_info:
+                # act
+                qt.mm1_model_compute_lq(lam = lam_input, miu = miu_input)
+            # assert
+            assert str(exception_info.value) == qt.UNSTABLE_MESSAGE
+
+
+    def test_mm1_model_compute_lq_negative_zero_input_raises_exception(self):
+        """
+        Test case for mm1_model_compute_lq function.
+
+        This function tests if some input have negative or zero values.
+        """
+
+        # arrange
+        input_cases = [(-10,10), (10,-10), (10,0), (0,10)]
+
+        for lam_input, miu_input in input_cases:
+            with raises(Exception) as exception_info:
+                # act
+                qt.mm1_model_compute_lq(lam = lam_input, miu = miu_input)
+            # assert
+            assert str(exception_info.value) == qt.NEGATIVE_INPUT_ERROR
+
+
     def test_mm1_model_compute_wq_expected_valid_output(self):
         """
         Test case for mm1_model_compute_wq function.
@@ -65,15 +119,15 @@ class TestMm1:
         # assert
         assert wq_calc != 0, "Wq is equal to 0"
         assert ma.isclose(wq_calc, expected_output,
-                          rel_tol=1e-9), "Wq is not approximately equal to 0.033"
+                          rel_tol=1e-9), "Wq is not approximately equal to 0.666..."
 
 
     def test_mm1_model_compute_wq_unstable_system_input_raises_exception(self):
         """
         Test case for mm1_model_compute_wq function.
 
-        This function tests the if there is input
-
+        This function test if the input raise an exception when the system is
+        unstable.
         """
         # arrange
         input_cases = [(10,10), (10,5)]
@@ -94,7 +148,7 @@ class TestMm1:
         """
 
         # arrange
-        input_cases = [(-10,10), (10,-10)]
+        input_cases = [(-10,10), (10,-10), (10,0), (0,10)]
 
         for lam_input, miu_input in input_cases:
             with raises(Exception) as exception_info:
@@ -108,7 +162,7 @@ class TestMm1:
         """
         Test case for mm1_model_compute_w function.
 
-        This function tests the calculation of Wq (expected waiting time in the
+        This function tests the calculation of w (expected waiting time in the
         queue) in the M/M/1 queueing model.
         """
         # arrange
@@ -120,17 +174,17 @@ class TestMm1:
         wq_calc = qt.mm1_model_compute_w(lam = lam_input, miu = miu_input)
 
         # assert
-        assert wq_calc != 0, "Wq is equal to 0"
+        assert wq_calc != 0, "w is equal to 0"
         assert ma.isclose(wq_calc, expected_output,
-                          rel_tol=1e-9), "Wq is not approximately equal to 0.033"
+                          rel_tol=1e-9), "w is not approximately equal to 0.1"
 
 
     def test_mm1_model_compute_w_unstable_system_input_raises_exception(self):
         """
         Test case for mm1_model_compute_w function.
 
-        This function tests the if there is input
-
+        This function tests if the input raise an exception when the system is
+        unstable.
         """
         # arrange
         input_cases = [(10,10), (10,5)]
@@ -151,7 +205,7 @@ class TestMm1:
         """
 
         # arrange
-        input_cases = [(-10,10), (10,-10)]
+        input_cases = [(-10,10), (10,-10), (10,0), (0,10)]
 
         for lam_input, miu_input in input_cases:
             with raises(Exception) as exception_info:
