2a35ca1f43853fb6f0b54b314aef3afabd286902
jop <jorge.diazsagot@ucr.ac.cr>
2023-12-27 14:47:36 -0600

    Add mm1_model_compute_w unit test

--- src/test_queueing_theory.py
+++ src/test_queueing_theory.py
@@ -102,3 +102,60 @@ class TestMm1:
                 qt.mm1_model_compute_wq(lam = lam_input, miu = miu_input)
             # assert
             assert str(exception_info.value) == qt.NEGATIVE_INPUT_ERROR
+
+
+    def test_mm1_model_compute_w_expected_valid_output(self):
+        """
+        Test case for mm1_model_compute_w function.
+
+        This function tests the calculation of Wq (expected waiting time in the
+        queue) in the M/M/1 queueing model.
+        """
+        # arrange
+        lam_input = 20
+        miu_input = 30
+        expected_output = 1/10
+
+        # act
+        wq_calc = qt.mm1_model_compute_w(lam = lam_input, miu = miu_input)
+
+        # assert
+        assert wq_calc != 0, "Wq is equal to 0"
+        assert ma.isclose(wq_calc, expected_output,
+                          rel_tol=1e-9), "Wq is not approximately equal to 0.033"
+
+
+    def test_mm1_model_compute_w_unstable_system_input_raises_exception(self):
+        """
+        Test case for mm1_model_compute_w function.
+
+        This function tests the if there is input
+
+        """
+        # arrange
+        input_cases = [(10,10), (10,5)]
+
+        for lam_input, miu_input in input_cases:
+            with raises(Exception) as exception_info:
+                # act
+                qt.mm1_model_compute_w(lam = lam_input, miu = miu_input)
+            # assert
+            assert str(exception_info.value) == qt.UNSTABLE_MESSAGE
+
+
+    def test_mm1_model_compute_w_negative_zero_input_raises_exception(self):
+        """
+        Test case for mm1_model_compute_w function.
+
+        This function tests if some input have negative or zero values.
+        """
+
+        # arrange
+        input_cases = [(-10,10), (10,-10)]
+
+        for lam_input, miu_input in input_cases:
+            with raises(Exception) as exception_info:
+                # act
+                qt.mm1_model_compute_w(lam = lam_input, miu = miu_input)
+            # assert
+            assert str(exception_info.value) == qt.NEGATIVE_INPUT_ERROR
