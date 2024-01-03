dc2f202779b8274bd13efd818971af225eb60506
Antonio Badilla Olivas <anthonny.badilla@ucr.ac.cr>
2024-01-03 10:51:07 +0100

    Add tests for the mms functions to compute l, p_n and model info

--- src/test_queueing_theory_mms.py
+++ src/test_queueing_theory_mms.py
@@ -140,3 +140,119 @@ class TestMMs:
         w_expected = 0.082051282
         # act => assert
         assert qt.mms_model_compute_w(lam, miu, s) == approx(w_expected)
+
+    # test compute l
+    # testing with invalid inputs
+    def test_mms_model_compute_l_negative_zero_input_raises_exception(self):
+        test_cases = [(-10, 10, 1), (10, -10, 1), (10, 10, 0)]
+        for lam, miu, s in test_cases:
+            with raises(NameError) as exception_info:
+                # act
+                qt.mms_model_compute_l(lam, miu, s)
+                # assert
+            assert (
+                str(exception_info.value) == qt.NEGATIVE_INPUT_ERROR
+            ), "Did not raise error with invalid inputs in compute l."
+
+    # testing for unstable systems
+    def test_mms_model_compute_l_unstable_system_raises_exception(self):
+        # arrange
+        lam, miu, s = 20, 10, 1
+        # act
+        with raises(NameError) as exception_info:
+            qt.mms_model_compute_l(lam, miu, s)
+        # assert
+        assert (
+            str(exception_info.value) == qt.UNSTABLE_MESSAGE
+        ), "Did not raise error when system is unstable in compute l."
+
+    # test compute p_n
+    # testing invalid inputs
+    def test_mms_model_compute_p_n_invalid_inputs_raise_exception(self):
+        # arrange
+        test_cases = [(-10, 10, 1, -1), (10, -10, 1, 2), (10, 10, 0, -3)]
+        for lam, miu, s, n in test_cases:
+            with raises(NameError) as exception_info:
+                # act
+                qt.mms_model_compute_p_n(lam, miu, s, n)
+                # assert
+            assert (str(exception_info.value) == qt.NEGATIVE_N_ERROR) or (
+                str(exception_info.value) == qt.NEGATIVE_INPUT_ERROR
+            ), "Did not raise error with invalid inputs in compute p_n."
+
+    # testing unstable system
+    def test_mms_model_compute_p_n_unstable_system_raises_exception(self):
+        lam, miu, s, n = 20, 10, 1, 2
+        with raises(NameError) as exception_info:
+            qt.mms_model_compute_p_n(lam, miu, s, n)
+        assert (
+            str(exception_info.value) == qt.UNSTABLE_MESSAGE
+        ), "Did not raise error when system is unstable in compute p_n."
+
+    # testing valid input stable system
+    def test_mms_model_compute_p_n_valid_input_stable_system(self):
+        # arrange
+        lam = 4
+        miu = 6
+        s = 2
+        p = [
+            0.5,
+            0.3333333,
+            0.11111111,
+            0.037037037,
+            0.0123456789,
+            0.004115226,
+            0.001371742,
+            0.000457247,
+            0.0001524157,
+        ]
+        for i in range(len(p)):
+            assert qt.mms_model_compute_p_n(lam, miu, s, i) == approx(
+                p[i]
+            ), "Did not compute expected probability in compute p_n."
+
+    # test model info
+    # testing invalid inputs
+    def test_mms_model_info_invalid_inputs_raise_exception(self):
+        test_cases = [(-10, 10, 1), (10, -10, 1), (10, 10, 0)]
+        for lam, miu, s in test_cases:
+            with raises(NameError) as exception_info:
+                # act
+                qt.mms_model_info(lam, miu, s)
+                # assert
+            assert (
+                str(exception_info.value) == qt.NEGATIVE_INPUT_ERROR
+            ), "Did not raise error with invalid inputs in model info."
+
+    # testing unstable system
+    def test_mms_model_info_unstable_system_raises_exception(self):
+        # arrange
+        lam, miu, s = 20, 10, 1
+        # act
+        with raises(NameError) as exception_info:
+            qt.mms_model_info(lam, miu, s)
+        # assert
+        assert (
+            str(exception_info.value) == qt.UNSTABLE_MESSAGE
+        ), "Did not raise error when system is unstable in model info."
+
+    # testing valid inputs stable system.
+    def test_mms_model_info_valid_input_stable_system_returns_expected(self):
+        # arrange
+        lam = 4
+        miu = 6
+        s = 2
+        p_zero = 0.5
+        rho = 0.3333333
+        l = 0.75
+        w = 0.1875
+        wq = 0.0208333333
+        lq = 0.08333333
+        expected_info = [rho, p_zero, lq, wq, w, l]
+        # act
+        computed_info = qt.mms_model_info(lam, miu, s)
+        # assert
+        for expected, computed in zip(expected_info, computed_info):
+            assert (
+                approx(expected) == computed
+            ), "Did not return the expected model info."
